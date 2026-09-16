20eaaa555cf6bc0364ddd3edfd460ca0a6d5804f
hrustinszkiadam <hrustinszkiadam@gmail.com>
2026-09-16 09:08:38 +0200

    feat: add tasks 4 through 7

--- L02/Program.cs
+++ L02/Program.cs
@@ -13,6 +13,18 @@ class Program
         TaskThree();
         CleanUp();
 
+        TaskFour();
+        CleanUp();
+
+        TaskFive();
+        CleanUp();
+
+        TaskSix();
+        CleanUp();
+
+        TaskSeven();
+        CleanUp();
+
         Console.WriteLine("\n\nNyomd meg az Enter-t a kilépéshez...");
         Console.ReadLine();
     }
@@ -103,5 +115,131 @@ class Program
             Console.Write($"{randomNumber}, ");
         } while (randomNumber != number);
         Console.WriteLine();
+    }
+
+    static void TaskFour()
+    {
+        PrintTaskHeader(4);
+
+        Console.Write("Add meg a játékosok számát: ");
+        int playerCount = int.Parse(Console.ReadLine() ?? "0");
+
+        if (playerCount <= 0)
+        {
+            Console.WriteLine("Ennyi játékos nem lehet.");
+            return;
+        }
+
+        Random random = new Random();
+        int highestRollPlayer = -1;
+        int highestRoll = 0;
+        Console.WriteLine("Eldöntjük, ki kezdjen. A játékosok sorszáma 1-től indul.");
+        for (int i = 1; i <= playerCount; i++)
+        {
+            Console.WriteLine($"Játékos {i} következik.");
+            Console.Write("Nyomd meg az Enter-t a dobáshoz...");
+            Console.ReadLine();
+            
+            Console.WriteLine($"Játékos {i} dob...");
+            int roll = random.Next(1, 7);
+            Console.WriteLine($"A dobott szám: {roll}");
+
+            if (roll == 6)
+            {
+                Console.WriteLine($"Játékos {i} kezdhet!");
+                return;
+            } else if (roll > highestRoll)
+            {
+                highestRoll = roll;
+                highestRollPlayer = i;
+            }
+        }
+        
+        Console.WriteLine($"A legtöbbet dobó játékos kezd: Játékos {highestRollPlayer} a {highestRoll} dobással.");
+
+    }
+
+    static void TaskFive()
+    {
+        PrintTaskHeader(5);
+
+        Random random = new Random();
+        int randomNumber = random.Next(1, 101);
+        int guess, attempts = 0;
+
+        Console.WriteLine("Gondoltam egy számra 1 és 100 között. Találd ki!");
+        do
+        {
+            Console.Write("Tippelj: ");
+            guess = int.Parse(Console.ReadLine() ?? "0");
+            attempts++;
+
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("A gondolt szám nagyobb.");
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("A gondolt szám kisebb.");
+            }
+        } while (guess != randomNumber);
+        Console.WriteLine($"Gratulálok! Eltaláltad a {randomNumber} számot {attempts} próbálkozásból.");
+    }
+
+    static void TaskSix()
+    {
+        PrintTaskHeader(6);
+
+        Console.Write("Adj meg egy pozitív egész számot: ");
+        int number = int.Parse(Console.ReadLine() ?? "0");
+        if (number <= 0)
+        {
+            Console.WriteLine("A megadott szám nem pozitív egész szám.");
+            return;
+        }
+
+        string pairity = (number % 2 == 0) ? "páros" : "páratlan";
+        Console.WriteLine($"A(z) {number} szám {pairity}.");
+
+        int numberOfDivisors = 0;
+        for (int i = 1; i <= number/2; i++)
+        {
+            if (number % i == 0)
+            {
+                numberOfDivisors++;
+            }
+        }
+        numberOfDivisors++; // include the number itself
+        Console.WriteLine($"A(z) {number} számnak {numberOfDivisors} osztója van.");
+
+        if (numberOfDivisors == 2)
+        {
+            Console.WriteLine($"A(z) {number} szám prímszám.");
+        }
+        else
+        {
+            Console.WriteLine($"A(z) {number} szám összetett szám.");
+        }
+    }
+
+    static void TaskSeven()
+    {
+        PrintTaskHeader(7);
+
+        Console.Write("Adj meg egy pozitív egész számot: ");
+        int number = int.Parse(Console.ReadLine() ?? "0");
+        if (number <= 0)
+        {
+            Console.WriteLine("A megadott szám nem pozitív egész szám.");
+            return;
+        }
+
+        long factorial = 1;
+        for (int i = 2; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"A(z) {number} szám faktoriálisa: {factorial}");
     }
 }
