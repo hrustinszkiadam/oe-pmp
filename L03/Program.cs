38695e26ff25cf7c20169df86acd70b991b689dc
hrustinszkiadam <hrustinszkiadam@gmail.com>
2026-09-23 08:22:51 +0200

    chore: setup

---
+++ L03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace L03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n\nNyomd meg az Enter-t a kilépéshez...");
+        Console.ReadLine();
+    }
+
+    static void CleanUp()
+    {
+        Console.WriteLine("\n\nNyomd meg az Enter-t a következő feladat elindításához...");
+        Console.ReadLine();
+        Console.Clear();
+    }
+
+    static void PrintTaskHeader(int taskNumber)
+    {
+        Console.WriteLine($"--- {taskNumber}. feladat ---\n");
+    }
+}
